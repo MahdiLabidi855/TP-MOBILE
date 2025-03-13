fcde857ad48f737edd541ef5b5ebf83b2e74fa84
MahdiLAABIDI <mahdi.laabidi@teamwillgroup.com>
2025-03-13 11:25:42 +0100

    tp2 ex2+ java

--- TP 2 ex 2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ TP 2 ex 2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText p,t;
+    RadioButton m,c;
+    Button calc,rez;
+    TextView res;
+    float r=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,42 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        p=findViewById(R.id.p);
+        t=findViewById(R.id.t);
+        m=findViewById(R.id.radio_metre);
+        c=findViewById(R.id.radio_centimètre);
+        calc=findViewById(R.id.btn_cal_imc);
+        rez=findViewById(R.id.btn_raz);
+        res=findViewById(R.id.txt_res);
+
+        calc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float p1=Float.valueOf(p.getText().toString().trim());
+                float t1=Float.valueOf(t.getText().toString().trim());
+                if(c.isChecked()){
+                     r= (float) (p1/(Math.pow(t1,2)));
+                }
+                if(m.isChecked()){
+                     r= (float) (p1/(Math.pow((t1/100),2)));
+                }
+                res.setText(String.valueOf(r));
+
+            }
+        });
+        rez.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                p.getText().clear();
+                t.getText().clear();
+                res.setText("Vous devez cliquer sur le bouton « Calculer l’IMC » pour obtenir un résultat.");
+
+
+
+            }
+        });
+
+
+
     }
 }
